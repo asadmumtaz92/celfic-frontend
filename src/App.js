6bf035f4fffb488a1b0081c62ee0b4ab873aed07
Asad Mumtaz <asadmumtaz92@gmail.com>
2023-07-28 01:41:11 +0500

    This commit for Update & Add new Route for app...

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Signup from './App/screens/Login/Signup'
 import ForgotPassword from './App/screens/Login/ForgotPassword'
 
 import HomeRoute from './App/screens/Home/HomeRoute'
-import ErrorPageHome from './App/screens/Home/ErrorPageHome'
+// import ErrorPageHome from './App/screens/Home/ErrorPageHome'
 import Home from './App/screens/Home/Home'
 import Profile from './App/screens/Home/Profile'
 
@@ -19,13 +19,47 @@ const router = createBrowserRouter([
         element: <MyRouteLayout />,
         errorElement: <ErrorPageLogin />,
         children: [
-            { path: '/', element: <Login /> },
+            { index: true, element: <Login /> },
             { path: '/login', element: <Login /> },
-            // { index: true, element: <Login /> },
             { path: '/signup', element: <Signup /> },
             { path: '/recover-password', element: <ForgotPassword /> },
-        ]
+            {
+                path: '/',
+                element: <HomeRoute />,
+                children: [
+                    { path: 'home', index: true, element: <Home /> },
+                    // { path: '/home', index: true, element: <Home />, loader: eventsLoader, action: eventAction,}
+                    { path: 'profile/:usename', element: <Profile /> },
+                    { path: 'globe', element: <h2 className='mt-5 pt-3 text-center mb-5'>This is globe page...</h2> },
+                    { path: 'notifications', element: <h2 className='mt-5 pt-3 text-center mb-5'>This is notifications page...</h2> },
+                    { path: 'settings', element: <h2 className='mt-5 pt-3 text-center mb-5'>This is setting page...</h2> },
+                    { path: 'challenges', element: <h2 className='mt-5 pt-3 text-center mb-5'>This is challenges page...</h2> },
+                    { path: 'new-friends', element: <h2 className='mt-5 pt-3 text-center mb-5'>This is new - friend page...</h2> },
+                    { path: 'post/:postid', element: <h2 className='mt-5 pt-3 text-center mb-5'>This is post detail page...</h2> },
+                    {
+                        path: 'chat',
+                        element: <HomeRoute />,
+                        children: [
+                            { path: '', element: <h2 className='mt-5 pt-3 text-center mb-5'>This is chat page...</h2> },
+                            { path: ':username', element: <h2 className='mt-5 pt-3 text-center mb-5'>This is single user chat page...</h2> },
+                        ]
+                    },
+                ]
+            },
+        ],
     },
+    {
+    //     path: '/',
+    //     element: <MyRouteLayout />,
+    //     errorElement: <ErrorPageLogin />,
+    //     children: [
+    //         { index:true, element: <Login /> },
+    //         { path: '/login', element: <Login /> },
+    //         // { index: true, element: <Login /> },
+    //         { path: '/signup', element: <Signup /> },
+    //         { path: '/recover-password', element: <ForgotPassword /> },
+    //     ]
+    // },
     // {
     //     path: '/home',
     //     element: <HomeRoute />,
@@ -36,53 +70,53 @@ const router = createBrowserRouter([
     //         { path: 'profile', element: <Profile />, errorElement: <ErrorPageHome /> }
     //     ]
     // },
-    {
-        path: '/home',
-        element: <HomeRoute />,
-        errorElement: <ErrorPageHome />,
-        children: [
-            { path: '/home', element: <Home />, errorElement: <ErrorPageHome /> }
-        ]
-    },
-    {
-        path: '/profile',
-        element: <HomeRoute />,
-        errorElement: <ErrorPageHome />,
-        children: [
-            { path: '/profile/:usename', element: <Profile />, errorElement: <ErrorPageHome /> },
-        ]
-    },
-    {
-        path: '/globe',
-        element: <HomeRoute />,
-        errorElement: <ErrorPageHome />,
-        children: [
-            { path: '/globe', element: <h2 className='mt-5 pt-3 text-center mb-5'>This is globe page...</h2>, errorElement: <ErrorPageHome />, },
-        ]
-    },
-    {
-        path: '/notifications',
-        element: <HomeRoute />,
-        errorElement: <ErrorPageHome />,
-        children: [
-            { path: '/notifications', element: <h2 className='mt-5 pt-3 text-center mb-5'>This is notifications page...</h2>, errorElement: <ErrorPageHome />, },
-        ]
-    },
-    {
-        path: '/chat',
-        element: <HomeRoute />,
-        errorElement: <ErrorPageHome />,
-        children: [
-            { path: '/chat', element: <h2 className='mt-5 pt-3 text-center mb-5'>This is chat page...</h2>, errorElement: <ErrorPageHome />, },
-        ]
-    },
-    {
-        path: '/settings',
-        element: <HomeRoute />,
-        errorElement: <ErrorPageHome />,
-        children: [
-            { path: '/settings', element: <h2 className='mt-5 pt-3 text-center mb-5'>This is setting page...</h2>, errorElement: <ErrorPageHome />, },
-        ]
+    // {
+    //     path: '/home',
+    //     element: <HomeRoute />,
+    //     errorElement: <ErrorPageHome />,
+    //     children: [
+    //         { path: '/home', element: <Home />, errorElement: <ErrorPageHome /> }
+    //     ]
+    // },
+    // {
+    //     path: '/profile',
+    //     element: <HomeRoute />,
+    //     errorElement: <ErrorPageHome />,
+    //     children: [
+    //         { path: '/profile/:usename', element: <Profile />, errorElement: <ErrorPageHome /> },
+    //     ]
+    // },
+    // {
+    //     path: '/globe',
+    //     element: <HomeRoute />,
+    //     errorElement: <ErrorPageHome />,
+    //     children: [
+    //         { path: '/globe', element: <h2 className='mt-5 pt-3 text-center mb-5'>This is globe page...</h2>, errorElement: <ErrorPageHome />, },
+    //     ]
+    // },
+    // {
+    //     path: '/notifications',
+    //     element: <HomeRoute />,
+    //     errorElement: <ErrorPageHome />,
+    //     children: [
+    //         { path: '/notifications', element: <h2 className='mt-5 pt-3 text-center mb-5'>This is notifications page...</h2>, errorElement: <ErrorPageHome />, },
+    //     ]
+    // },
+    // {
+    //     path: '/chat',
+    //     element: <HomeRoute />,
+    //     errorElement: <ErrorPageHome />,
+    //     children: [
+    //         { path: '/chat', element: <h2 className='mt-5 pt-3 text-center mb-5'>This is chat page...</h2>, errorElement: <ErrorPageHome />, },
+    //     ]
+    // },
+    // {
+    //     path: '/settings',
+    //     element: <HomeRoute />,
+    //     errorElement: <ErrorPageHome />,
+    //     children: [
+    //         { path: '/settings', element: <h2 className='mt-5 pt-3 text-center mb-5'>This is setting page...</h2>, errorElement: <ErrorPageHome />, },
+    //     ]
     },
 ])
 
